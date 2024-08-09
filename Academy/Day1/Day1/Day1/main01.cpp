e3e2de2da32f7f9dd3afb3645ad5b2295a5021d5
pchanbro <cksgud0509@naver.com>
2024-08-09 21:21:36 +0900

    세팅

---
+++ Academy/Day1/Day1/Day1/main01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void main() 
+{
+	printf("동해물과 백두산이 마르고 닳도록\n");
+	printf("하느님이 보우하사 우리나라 만세\n");
+	printf("무궁화 삼천리 화려 강산\n");
+	printf("대한 사람 대한으로 길이 보전하세\n");
+}
+
+//#include <iostream>
+// iostream << [input output] stream을 다루는 내용을 포함
+// 

94b48d6a09af9e9f0c1e4024b2b098f2fc92ef18
langho968 <97336364+langho968@users.noreply.github.com>
2024-09-25 18:45:13 +0900

    Day 13 숙제

---
+++ Academy/Day12/BinaryTree/BinaryTree/BinaryTree.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+
+struct Node
+{
+	Node* Parent;
+	Node* Left;
+	Node* Right;
+
+	int Data;
+};
+
+class BinaryTree
+{
+public:
+	void AddData(int data);
+	void RemonveData(int data);
+	Node* GetNode(int data);
+
+private:
+	Node* _root = nullptr;
+};
+
+void BinaryTree::AddData(int data)
+{
+
+}
+void BinaryTree::RemonveData(int data)
+{
+
+}
+Node* BinaryTree::GetNode(int data)
+{
+
+}
+
+	
